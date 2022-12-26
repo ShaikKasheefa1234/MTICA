525596b2d2ddf3ae2e681aaedb9fefc98d96bf34
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-26 16:59:13 +0530

    Add files via upload

---
+++ programshell.py
@@ -0,0 +1,303 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/pythonpractice25/Day5/program1.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program1.py", line 3, in <module>
+    print(Z)
+NameError: name 'Z' is not defined. Did you mean: 'z'?
+
+================= RESTART: D:/pythonpractice25/Day5/program1.py ================
+256
+-8
+
+================= RESTART: D:/pythonpractice25/Day5/program2.py ================
+Hello Wrold
+value of x is value of y is 0
+
+================= RESTART: D:/pythonpractice25/Day5/program2.py ================
+Hello Wrold
+value of x is 8 value of y is 0
+
+================= RESTART: D:/pythonpractice25/Day5/program2.py ================
+Hello Wrold
+value of x is 8 value of y is 10
+
+================= RESTART: D:/pythonpractice25/Day5/program6.py ================
+cats	are
+	good	sources
+		of	internet	memes
+s
+'cats\tare\n\tgood\tsources\n\t\tof\tinternet\tmemes'
+
+================= RESTART: D:/pythonpractice25/Day5/program3.py ================
+s1
+'This is a multi-line string'
+print(s1)
+This is a multi-line string
+
+================= RESTART: D:/pythonpractice25/Day5/program3.py ================
+s1
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    s1
+NameError: name 's1' is not defined
+print(s1)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(s1)
+NameError: name 's1' is not defined
+
+================= RESTART: D:/pythonpractice25/Day5/program3.py ================
+s1
+'This is a multi-line string'
+
+================= RESTART: D:/pythonpractice25/Day5/program3.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program3.py", line 4, in <module>
+    print(s1)
+NameError: name 's1' is not defined
+print(s1)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print(s1)
+NameError: name 's1' is not defined
+
+================= RESTART: D:/pythonpractice25/Day5/program3.py ================
+print(s1)
+This is a multi-line string
+
+================= RESTART: D:/pythonpractice25/Day5/program7.py ================
+\\\\
+\
+\
+\
+
+Good-bye
+
+================= RESTART: D:/pythonpractice25/Day5/program4.py ================
+s0: *	*
+**	**
+***	***
+
+s1: I said,"This is a valid string."
+
+================= RESTART: D:/pythonpractice25/Day5/program4.py ================
+s0:
+ *	*
+**	**
+***	***
+
+s1:
+ I said,"This is a valid string."
+
+================= RESTART: D:/pythonpractice25/Day5/program8.py ================
+abc abc abc abc 
+s1
+'abcabcabcabc'
+s2
+'abc abc abc abc '
+x+y
+'abcdef'
+
+================ RESTART: D:/pythonpractice25/Day5/program11.py ================
+Enter an integer==> 4
+x is: 88
+
+================ RESTART: D:/pythonpractice25/Day5/program12.py ================
+Enter an integer==> 
+================ RESTART: D:/pythonpractice25/Day5/program12.py ================
+Enter an integer==> 
+================ RESTART: D:/pythonpractice25/Day5/program12.py ================
+Enter an integer==> 
+================ RESTART: D:/pythonpractice25/Day5/program12.py ================
+Enter an integer ==> 64
+64 , 6 6
+
+================ RESTART: D:/pythonpractice25/Day5/program11.py ================
+Enter an integer==> 4
+x is: 88
+
+================= RESTART: D:/pythonpractice25/Day5/program5.py ================
+s1
+'"Hi mom",I said."How are you?"'
+s2
+'"Hi mom",I said.How are you?'
+s3
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    s3
+NameError: name 's3' is not defined. Did you mean: 's1'?
+s5
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    s5
+NameError: name 's5' is not defined. Did you mean: 's1'?
+s4
+'\'Hi mom",I said.\'"How are you?"\''
+s6
+'" is this a cheese shop?"\n\t"we have all kinds!"'
+
+================= RESTART: D:/pythonpractice25/Day5/program5.py ================
+s6
+'" is this a cheese shop?"\n\t"we have all kinds!"'
+
+================= RESTART: D:/pythonpractice25/Day5/program9.py ================
+6
+3
+
+================= RESTART: D:/pythonpractice25/Day5/program9.py ================
+6
+3
+8
+print(s)
+Hi
+sis!
+
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 2, in <module>
+    'abc' * str(5)
+TypeError: can't multiply sequence by non-int of type 'str'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 2, in <module>
+    'abc' *str(5)
+TypeError: can't multiply sequence by non-int of type 'str'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc' +5
+TypeError: can only concatenate str (not "int") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 2, in <module>
+    'abc'*str(5)
+TypeError: can't multiply sequence by non-int of type 'str'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 2, in <module>
+    'abc'*str(5)
+TypeError: can't multiply sequence by non-int of type 'str'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 5, in <module>
+    'abc'+5.0
+TypeError: can only concatenate str (not "float") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 5, in <module>
+    'abc'+5.0
+TypeError: can only concatenate str (not "float") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 6, in <module>
+    'abc'+float(5.0)
+TypeError: can only concatenate str (not "float") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+str(3.0)*3
+'3.03.03.0'
+'abc'+5.0
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    'abc'+5.0
+TypeError: can only concatenate str (not "float") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+'abc'+str(5)
+'abc5'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 2, in <module>
+    'abc'*str(5)
+TypeError: can't multiply sequence by non-int of type 'str'
+'abc'+str(5)
+'abc5'
+'abc'*str(5)
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    'abc'*str(5)
+TypeError: can't multiply sequence by non-int of type 'str'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+'abc'+5
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+'abc'+5
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+'abc'*5
+'abcabcabcabcabc'
+
+================ RESTART: D:/pythonpractice25/Day5/program10.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program10.py", line 3, in <module>
+    'abc'+5
+TypeError: can only concatenate str (not "int") to str
+'abc'+5.0
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    'abc'+5.0
+TypeError: can only concatenate str (not "float") to str
+str(3.0)*3
+'3.03.03.0'
+
+================ RESTART: D:\pythonpractice25\Day5\program12.py ================
+Enter an integer ==> 10
+10 , 1 1
+
+================ RESTART: D:\pythonpractice25\Day5\program12.py ================
+Enter an integer ==> 64
+64 , 6 6
+
+================ RESTART: D:\pythonpractice25\Day5\program12.py ================
+Enter an integer ==> 64
+64,66
