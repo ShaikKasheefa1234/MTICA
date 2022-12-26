efecd47416149be116ffafafc069e84bd3b7b13c
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-26 16:59:27 +0530

    Add files via upload

---
+++ programshell1.py
@@ -0,0 +1,183 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a='apple';b='paple'
+a
+'apple'
+b
+'paple'
+sorted(a)
+['a', 'e', 'l', 'p', 'p']
+sorted(b)
+['a', 'e', 'l', 'p', 'p']
+'apple'.split()
+['apple']
+'apple paple'.split()
+['apple', 'paple']
+'a,p,p,l,e'.split(',')
+['a', 'p', 'p', 'l', 'e']
+'app.le.is.sweet'.split('.')
+['app', 'le', 'is', 'sweet']
+'today is monday'.split(' ')
+['today', 'is', 'monday']
+'today is monday'.split()
+['today', 'is', 'monday']
+'computer'.split(',')
+['computer']
+'apple mango'.split()
+['apple', 'mango']
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+10 20
+no
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+20 40
+no
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+0 1
+no
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+mca cam
+yes
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+mca acm
+yes
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+mca cmd
+no
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+inp
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program13.py", line 7, in <module>
+    print(checkAnagram(inp[0],inp[1]))
+IndexError: list index out of range
+inp
+['inp']
+cam dam
+SyntaxError: invalid syntax
+
+================ RESTART: D:/pythonpractice25/Day5/program13.py ================
+cam dam
+no
+inp
+['cam', 'dam']
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+s1
+Vowels: 
+print(Vowels)
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    print(Vowels)
+NameError: name 'Vowels' is not defined
+today Ganguly is energetic
+SyntaxError: invalid syntax
+today ganguly is energetic
+SyntaxError: invalid syntax
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+today Ganguly is Energetic
+Vowels: oaauiEeei
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+today Ganguly is Energetic
+i= t
+i= o
+i= d
+i= a
+i= y
+i=  
+i= G
+i= a
+i= n
+i= g
+i= u
+i= l
+i= y
+i=  
+i= i
+i= s
+i=  
+i= E
+i= n
+i= e
+i= r
+i= g
+i= e
+i= t
+i= i
+i= c
+Vowels: oaauiEeei
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program14.py", line 11, in <module>
+    str1=input()
+KeyboardInterrupt
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+Traceback (most recent call last):
+  File "D:/pythonpractice25/Day5/program14.py", line 11, in <module>
+    str1=input()
+KeyboardInterrupt
+KeyboardInterrupt
+today Ganguly is Energetic
+SyntaxError: invalid syntax
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+today Ganguly is Energetic
+Vowels: oaauiEeei
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+today Ganguly is Energetic
+i= t
+i= o
+i= d
+i= a
+i= y
+i=  
+i= G
+i= a
+i= n
+i= g
+i= u
+i= l
+i= y
+i=  
+i= i
+i= s
+i=  
+i= E
+i= n
+i= e
+i= r
+i= g
+i= e
+i= t
+i= i
+i= c
+Vowels: oaauiEeei
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+today Ganguly is Energetic
+i: o temp_vowel: o
+i: a temp_vowel: oa
+i: a temp_vowel: oaa
+i: u temp_vowel: oaau
+i: i temp_vowel: oaaui
+i: E temp_vowel: oaauiE
+i: e temp_vowel: oaauiEe
+i: e temp_vowel: oaauiEee
+i: i temp_vowel: oaauiEeei
+Vowels: oaauiEeei
+
+================ RESTART: D:/pythonpractice25/Day5/program14.py ================
+today Ganguly is Energetic
+Vowels: oaauiEeei
