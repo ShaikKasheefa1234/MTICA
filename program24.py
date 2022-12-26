4b659630319fab26d70387c909f3bf8510a68376
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-26 17:28:19 +0530

    Add files via upload

--- program24.py
+++ program24.py
@@ -1,11 +1,4 @@
-def remove_duplicates(s):
-    str2=''
-    for i in s:
-        if i not in str2:
-            str2 +=i
-            #print (str2)
-    return str2
-        
-str1=input("Enter your text: ")
-print("Without duplicates: ",remove_duplicates(str1))
-                                                   
+n=int(input())
+#res='10' * (n-1) + '1'
+res='1' + '01' * (n-1)
+print(res)
