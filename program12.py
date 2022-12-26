b4611bd287bb7c7871badcbc074dedc331778d4d
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-26 13:21:14 +0530

    Add files via upload

---
+++ program12.py
@@ -0,0 +1,14 @@
+'''#x= input('Enter an integer ==> '), sep=''
+x = int(input('Enter an integer ==> '))
+y = x // 10
+z = y % 10
+print(x, ',', y, z)'''
+
+
+
+
+#x= input('Enter an integer ==> '), sep=''
+x = int(input('Enter an integer ==> '))
+y = x // 10
+z = y % 10
+print(x, ',', y, z, sep='')
