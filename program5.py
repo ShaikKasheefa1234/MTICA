541fc8575160fcaa0e37881b5ece02004c81c2c5
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-26 12:47:42 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,6 @@
+s1='"Hi mom",I said."How are you?"'
+s2='"Hi mom",I said.'"How are you?"
+#s3='"Hi mom',I said.'"How are you?"'
+s4="""'Hi mom",I said.'"How are you?"'"""
+#s5=""I want to be a lion tamer!"'
+s6="\" is this a cheese shop?\"\n\t\"we have all kinds!\""
