20773e010cbd0479c08c434ca9b82a437667a2b8
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-28 16:49:08 +0530

    Add files via upload

--- program13.py
+++ program13.py
@@ -1,7 +1,4 @@
-def checkAnagram(s1,s2):
-    if sorted(s1)==sorted(s2):
-        return 'yes'
-    else:
-        return 'no'
-inp=input().split()
-print(checkAnagram(inp[0],inp[1]))
+Lst=[10,15,20,25,30,35,40,45]
+ans=[i*i for i in Lst]
+print(ans)
+
