64e648f7f08c054064e7e07dd98fc20172c195cc
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-28 14:55:18 +0530

    Add files via upload

--- program6.py
+++ program6.py
@@ -1,3 +1,8 @@
-s="cats\tare\n\tgood\tsources\n\t\tof\tinternet\tmemes"
-#>>>s
-print(s)
+ans=[]
+for i in range(100,111):
+    temp=[]
+    for j in range(1,10):
+        if i%j==0:
+            temp.append(j)
+    ans.append([i,max(temp)])
+print(ans)
