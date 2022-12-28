cb06cba5c922e388e73b6810f5063c9d452b6f98
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-28 14:56:03 +0530

    Add files via upload

--- program9.py
+++ program9.py
@@ -1,6 +1,40 @@
-print(len('George'))
-print(len('Tom'))
-s="""Hi
-sis!
-"""
-print(len(s))
+#find transpose of a matrix
+'''m=[[1,2],[3,4],[5,6],[7,8]]
+ans=[]
+for row in range(len(m[0])):#row=0,1
+    temp=[]
+    for col in range(len(m)):#col=0,1,2,3
+        temp.append(m[col][row])
+    ans.append(temp)
+print(ans)'''
+
+
+
+'''m=[[1,2],[3,4],[5,6],[7,8]]
+ans=[]
+for row in range(len(m[0])):#row=0,1
+    temp=[(m[col][row]) for col in range(len(m))]
+    ans.append(temp)
+print(ans)'''
+
+
+
+'''m=[[1,2],[3,4],[5,6],[7,8]]
+ans=[]
+for row in range(len(m[0])):#row=0,1
+    ans.append([(m[col][row]) for col in range(len(m))])
+print(ans)'''
+
+
+m=[[1,2],[3,4],[5,6],[7,8]]
+ans=[(m[col][row]) for col in range(len(m))for row in range(len(m[0]))]
+#for row in range(len(m[0])):#row=0,1
+    #ans.append([(m[col][row]) for col in range(len(m))])
+print(ans)
+
+
+
+
+
+
+
