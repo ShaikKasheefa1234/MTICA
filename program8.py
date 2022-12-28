4c17ddbd35b6c887feed47352e3d4a0397f9a7ab
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-28 14:55:40 +0530

    Add files via upload

--- program8.py
+++ program8.py
@@ -1,11 +1,3 @@
-'abc' 'def'
-'abc' + 'def'
-'abc ' + 'def'
-x='abc'
-y='def'
-x+y
-#x y
-s1='abc'*4
-s1
-s2='abc '*4
-print(s2)
+ans=[ [i,max([j for j in range(1,11)if i%j==0])]
+    for i in range(100,110)]
+print(ans)
