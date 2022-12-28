08550a08f9eb355e777cdf3f6dd18706a153845c
ShaikKasheefa1234 <121174030+ShaikKasheefa1234@users.noreply.github.com>
2022-12-28 14:55:04 +0530

    Add files via upload

--- program5.py
+++ program5.py
@@ -1,6 +1,2 @@
-s1='"Hi mom",I said."How are you?"'
-s2='"Hi mom",I said.'"How are you?"
-#s3='"Hi mom',I said.'"How are you?"'
-s4="""'Hi mom",I said.'"How are you?"'"""
-#s5=""I want to be a lion tamer!"'
-s6="\" is this a cheese shop?\"\n\t\"we have all kinds!\""
+ans={i for i in range(1,101)for j in range(2,10)if i%j==0}
+print(ans)
